8596d5545a6bf98847cab33150cbf44e411feb2b
Danielirik <162011788+Danielirik@users.noreply.github.com>
2024-03-27 10:54:38 +0100

    Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+a simple bot,
+    dev:"customs team ltd"
+our contact info:email:"addyanenterprise@gmail.com"
+number:"+2348135320183"
+   do not edit thread info
+ a simple facebook messenger bot by customs team, 
+
+
+   support use to keep this bot work by helping us financially 
+bank:"8135320183 bank name palmpay"
+
